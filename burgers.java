dc38db34628941573ca27a7a7a80926c15ff3c60
Din2003 <nicoandgian@gmail.com>
2026-02-12 09:01:12 +0800

    commit

---
+++ burgers.java
@@ -0,0 +1,17 @@
+public class burgers {
+
+    public static void main(String[] args) {
+
+        //add x and y first, then subtract the sum by z
+
+        int x=6;
+        int y=5;
+        int z=3;
+
+        int home=x+y;
+        int house=home-z;
+
+        
+        System.out.println(house);
+    }
+}
