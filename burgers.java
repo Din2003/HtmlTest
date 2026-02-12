621acb9b68770c63fc49f71b083aeaeb3929c6f7
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2026-02-12 10:28:31 +0800

    added a comment

--- burgers.java
+++ burgers.java
@@ -2,16 +2,17 @@ public class burgers {
 
     public static void main(String[] args) {
 
-        //add x and y first, then subtract the sum by z
+        // add x and y first, then subtract the sum by z
 
-        int x=6;
-        int y=5;
-        int z=3;
+        int x = 6;
+        int y = 5;
+        int z = 3;
 
-        int home=x+y;
-        int house=home-z;
+        int home = x + y;
+        int house = home - z;
 
-        
         System.out.println(house);
+
+        // This is from Din
     }
 }
