558d2a9f4bd0ef575300424d7f41bf7536ea33a0
Din2003 <nicoandgian@gmail.com>
2026-02-16 14:59:19 +0800

    added guko.java

---
+++ guko.java
@@ -0,0 +1,17 @@
+public class guko{
+
+    public static void main(String[] args) {
+        // create variables x , y and z then assign them a number
+        // add x and y, then minus the sum by z
+        
+        int x = 4;
+        int y = 6;
+        int z = 3;
+
+        int e = x+y;
+        int p = e-z;
+
+        System.out.println(p);
+    }
+
+}
